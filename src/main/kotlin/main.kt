7be86aabfb680500e090c5e32fb8e40287e139ad
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-02-25 19:21:00 -0300

    Update main.kt

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,11 +3,14 @@
     println("Cidade: $cidade, $estado - $cep")
 }
 */
-fun media(n1:Float, n2:Float){
+fun media(vararg notas: Int){
+    if(notas.isNotEmpty()){
+
+    }
 
 }
 fun main()  {
-    media(8f,6f)
+    media(8,6,4,4,35,6,76,4,2,4,4,2,432,23,1312,23,345,234,123,12312)
     /*
     endereco(cidade="Campinas", estado ="São Paulo",cep ="13023-320")
     endereco(estado ="São Paulo", cidade="Campinas",cep ="13023-320")
