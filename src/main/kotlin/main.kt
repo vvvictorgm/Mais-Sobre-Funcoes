325ca7197339793571143dd85eea64923587c09c
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-02-25 19:18:52 -0300

    teste

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,7 +3,11 @@
     println("Cidade: $cidade, $estado - $cep")
 }
 */
+fun media(n1:Float, n2:Float){
+
+}
 fun main()  {
+    media(8f,6f)
     /*
     endereco(cidade="Campinas", estado ="São Paulo",cep ="13023-320")
     endereco(estado ="São Paulo", cidade="Campinas",cep ="13023-320")
