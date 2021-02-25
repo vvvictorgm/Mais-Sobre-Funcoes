b019c16b3924bc26e5e20ad0528406c1049f90c7
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-02-25 19:16:34 -0300

    UPDATE

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,10 @@
-fun endereco(rua: String, cidade: String, estado: String, cep: String){
+fun endereco(rua: String = "", cidade: String, estado: String, cep: String, numero: Int =0){
     println("Rua: $rua")
     println("Cidade: $cidade, $estado - $cep")
 }
 
 fun main()  {
+    endereco(cidade="Campinas", estado ="São Paulo",cep ="13023-320")
+    endereco(estado ="São Paulo", cidade="Campinas",cep ="13023-320")
+    endereco(cep ="13023-320",cidade="Campinas", estado ="São Paulo")
 }
