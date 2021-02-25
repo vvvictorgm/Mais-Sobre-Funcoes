a8cda61a27e54a84623c600b8f585c7ba14520dd
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-02-25 19:25:47 -0300

    Update main.kt

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -3,7 +3,7 @@
     println("Cidade: $cidade, $estado - $cep")
 }
 */
-fun media(vararg notas: Int){
+fun media(vararg notas: Float){
     if(notas.isNotEmpty()){
         var soma = 0f
         for(nota in notas){
@@ -14,8 +14,12 @@ fun media(vararg notas: Int){
     }
 
 }
+
+fun <T>media3(vararg valores: T ){
+
+}
 fun main()  {
-    media(8,6,4,4,35,6,76,4,2,4,4,2,432,23,1312,23,345,234,123,12312)
+    media(8f,6f,4f,4f,35f,6f,76f,4f,2f,4f,4f,2f,432f,23f,1312f,23f,345f,234f,123f,12312f)   
     /*
     endereco(cidade="Campinas", estado ="São Paulo",cep ="13023-320")
     endereco(estado ="São Paulo", cidade="Campinas",cep ="13023-320")
