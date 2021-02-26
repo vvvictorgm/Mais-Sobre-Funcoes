f668f2ea580374c329860310f9104677498c3be0
Victor Augusto Nithack Frazatto <victorfrazatto@hotmail.com>
2021-02-26 18:11:24 -0300

    Update main.kt

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -19,7 +19,7 @@ fun <T>media3(vararg valores: T ){
 
 }
 fun main()  {
-    media(8f,6f,4f,4f,35f,6f,76f,4f,2f,4f,4f,2f,432f,23f,1312f,23f,345f,234f,123f,12312f)   
+    media(8f,6f,4f,4f,35f,6f,76f,4f,2f,4f,4f,2f,432f,23f,1312f,23f,345f,234f,123f,12312f)
     /*
     endereco(cidade="Campinas", estado ="São Paulo",cep ="13023-320")
     endereco(estado ="São Paulo", cidade="Campinas",cep ="13023-320")
